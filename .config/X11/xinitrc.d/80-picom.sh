8784ed980d44c8c681850efb5ec6c543d2890a55
Rustam Uzairov <svrvt.zrv@gmail.com>
2024-09-11 20:03:49 +0300

    if --animations..

--- .config/X11/xinitrc.d/80-picom.sh
+++ .config/X11/xinitrc.d/80-picom.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 if command -v picom >/dev/null 2>&1; then
-   picom --daemon --config ~/.config/picom/picom.conf &
+   if picom -h | grep -q '\-\-animations'; then
+      picom --animations --daemon --config ~/.config/picom/picom.conf &
+   else
+      picom --daemon --config ~/.config/picom/picom.conf &
+   fi
 fi
-
