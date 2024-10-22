0780d024e57572a20440a1a1147ac71105462683
Rustam Uzairov <svrvt.zrv@gmail.com>
2024-10-22 21:42:18 +0300

    set animations if total gpu memory..

--- .config/X11/xinitrc.d/80-picom.sh
+++ .config/X11/xinitrc.d/80-picom.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 if command -v picom >/dev/null 2>&1; then
-   if picom -h | grep -q '\-\-animations'; then
-      picom --animations --daemon --config ~/.config/picom/picom.conf &
+   if command -v nvidia-smi >/dev/null 2>&1; then
+      tm=$(nvidia-smi --query-gpu=memory.total --format=csv | grep -v memory | awk '{print $1}')
+      if picom -h | grep -q '\-\-animations' && [[ $tm -ge 2048 ]]; then
+         picom --animations --daemon --config ~/.config/picom/picom.conf &
+      else
+         picom --daemon --config ~/.config/picom/picom.conf &
+      fi
    else
       picom --daemon --config ~/.config/picom/picom.conf &
    fi
