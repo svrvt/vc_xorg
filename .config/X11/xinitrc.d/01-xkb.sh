3ba7e3630f0e64e76521245ea0fe2f5612b22041
Rustam Uzairov <svrvt.zrv@gmail.com>
2024-10-22 21:18:40 +0300

    set keyboard

--- .config/X11/xinitrc.d/01-xkb.sh
+++ .config/X11/xinitrc.d/01-xkb.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
 #setxkbmap -model pc104 -layout us,ru -option grp:shifts_toggle &
-setxkbmap -option grp:alt_shift_toggle -layout us,ru &
 
+xkb_option=(\
+	'grp:lalt_lshift_toggle' \
+	'grp:switch' \
+)
+
+if localectl list-x11-keymap-options | grep -q 'caps:tmux_prefix'; then
+	xkb_option=("${xkb_option[@]}" 'caps:tmux_prefix')
+fi
+
+option_part=()
+
+for o in "${xkb_option[@]}"; do
+	option_part+=(-option "$o" )
+done
+
+setxkbmap "${option_part[@]}" -layout us,ru &
