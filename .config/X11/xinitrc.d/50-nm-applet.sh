9ed099ecbff1e49945902cec1cd6b3cbb17c54db
Rustam Uzairov <svrvt.zrv@gmail.com>
2025-07-23 16:35:42 +0300

    fix running nm-applet

--- .config/X11/xinitrc.d/50-nm-applet.sh
+++ .config/X11/xinitrc.d/50-nm-applet.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if ! systemctl -q is-active NetworkManager.service && \
+if systemctl -q is-active NetworkManager.service && \
   command -v nm-applet >/dev/null 2>&1; then
     nm-applet &
 fi
